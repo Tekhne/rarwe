efa24ec4c02347db2bc623e05310c6fd4b852bff
Peter Havens <Peter.Havens@optum.com>
2016-01-25 15:55:32 -0700

    Add new song to list.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -35,4 +35,16 @@ songs.get('content').pushObject(blackDog)
 songs.get('content').pushObject(yellowLedbetter);
 songs.get('content').pushObject(pretender);
 
-export default Ember.Controller.extend({ songs: songs });
+window.songs = songs;
+
+var alwaysWaiting = Song.create({
+  title: 'Always Waiting',
+  band: 'Kaya Project',
+  rating: 5
+});
+
+window.newSong = alwaysWaiting;
+
+export default Ember.Controller.extend({
+  songs: songs
+});
