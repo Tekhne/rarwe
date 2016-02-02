2e0320d8c743e525a896707c37aee2ae2e7b08c2
Peter Havens <Peter.Havens@optum.com>
2016-02-02 14:15:36 -0700

    Re-organize songs and bands.

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,14 +1,63 @@
 import Ember from 'ember';
 
-var Band = Ember.Object.extend({
-  name: '',
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
 });
 
-var ledZeppelin = Band.create({ name: 'Led Zeppelin' });
-var pearlJam = Band.create({ name: 'Pearl Jam' });
-var fooFighters = Band.create({ name: 'Foo Fighters' });
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zeppelin',
+  rating: 3
+});
 
-var bands = [ledZeppelin, pearlJam, fooFighters];
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var daughter = Song.create({
+  title: 'Daughter',
+  band: 'Pearl Jam',
+  rating: 5
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var Band = Ember.Object.extend({
+  name: '',
+  songs: []
+});
+
+var ledZeppelin = Band.create({
+  name: 'Led Zeppelin',
+  songs: [blackDog]
+});
+
+var pearlJam = Band.create({
+  name: 'Pearl Jam',
+  songs: [daughter, yellowLedbetter]
+});
+
+var fooFighters = Band.create({
+  name: 'Foo Fighters',
+  songs: [pretender]
+});
+
+var BandsCollection = Ember.Object.extend({
+  content: [],
+  sortedContent: Ember.computed.sort('content', 'sortProperties'),
+  sortProperties: ['name:desc']
+});
+
+var bands = BandsCollection.create();
+bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
 
 export default Ember.Route.extend({
   model: function() {
