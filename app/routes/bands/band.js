a052836b1af99841499b9ac566f34b82fe320471
Peter Havens <Peter.Havens@optum.com>
2016-02-04 15:58:44 -0700

    Obtain correct bands model.

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return bands;
+    var bands = this.modelFor('bands');
+    return bands.get('content').findBy('slug', params.slug);
   }
 });
