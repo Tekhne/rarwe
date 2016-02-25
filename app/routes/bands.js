1e6a8822768418a8d3cbbd875b9f4b42b14b7e5f
Peter Havens <Peter.Havens@optum.com>
2016-02-25 12:39:59 -0700

    Create a band using ember-data.

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -11,11 +11,14 @@ export default Ember.Route.extend({
     },
 
     createBand: function () {
-      var name = this.get('controller').get('name');
-      var band = Band.create({ name: name });
-      bands.get('content').pushObject(band);
-      this.get('controller').set('name', '');
-      this.transitionTo('bands.band.songs', band);
+      var route = this;
+      var controller = this.get('controller');
+      var band = this.store.createRecord('band', controller.getProperties('name'));
+
+      band.save().then(function () {
+        controller.set('name', '');
+        route.transitionTo('bands.band.songs', band);
+      });
     }
   }
 });
