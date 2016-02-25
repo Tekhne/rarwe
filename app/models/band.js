20c74a3879ba1830ff59e1d2950515761888c75c
Peter Havens <Peter.Havens@optum.com>
2016-02-25 11:06:17 -0700

    Setup band model to use ember-data.

--- app/models/band.js
+++ app/models/band.js
@@ -1,14 +1,10 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Object.extend({
-  name: '',
-  description: '',
-
-  setupSongs: Ember.on('init', function () {
-    if (!this.get('songs')) {
-      this.set('songs', []);
-    }
-  }),
+  name:        DS.attr('string'),
+  description: DS.attr(),
+  songs:       DS.hasMany('song'),
 
   slug: Ember.computed('name', function () {
     return this.get('name').dasherize();
