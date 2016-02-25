3fdc027f9c94d25b924868a279ed981e92e0a2d6
Peter Havens <Peter.Havens@optum.com>
2016-02-25 15:05:06 -0700

    Test that bands are listed on the /bands route.

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -3,10 +3,12 @@ import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | bands');
 
-test('visiting /bands', function(assert) {
+test('List bands', function (assert) {
   visit('/bands');
 
-  andThen(function() {
-    assert.equal(currentURL(), '/bands');
+  andThen(function () {
+    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+    assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
+    assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 'The other band link contains the band name');
   });
 });
