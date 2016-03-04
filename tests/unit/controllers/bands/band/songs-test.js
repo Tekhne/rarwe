da01593e64e0cfd82056584fe5ca414af907d3ba
Peter Havens <peter.havens@optum.com>
2016-03-04 12:34:54 -0700

    Update tests.

--- tests/unit/controllers/bands/band/songs-test.js
+++ tests/unit/controllers/bands/band/songs-test.js
@@ -14,9 +14,12 @@ test('canCreateSong', function (assert) {
   controller.set('model', band);
   controller.set('songCreationStarted', false);
   assert.ok(!controller.get('canCreateSong'), "Can't create song if process has not started and no songs yet");
+
   controller.set('songCreationStarted', true);
-  assert.ok(controller.get('canCreateSong'), 'Can create song if process has started');
-  controller.set('songCreationStarted', false);
+  controller.set('songCreationStarted', true);
+  assert.ok(controller.get('canCreateSong'), "Can create song if process has started");
+
+  controller.set('songCreationSTarted', false);
   band.set('songs', songs);
-  assert.ok(controller.get('canCreateSong'), 'Can create song if process has not started but there are already songs');
+  assert.ok(controller.get('canCreateSong'), "Can create song if process has not started but there are already songs");
 });
