57d4d4792c646f4f94fcf6b03fa8a6f53eccb30f
Peter Havens <peter.havens@optum.com>
2016-03-07 13:59:06 -0700

    Wire up sorting functionality.

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -19,6 +19,10 @@ export default Ember.Controller.extend({
 
       song.set('rating', rating);
       song.save();
+    },
+
+    setSorting: function (option) {
+      this.set('sortBy', option);
     }
   },
 
@@ -30,6 +34,18 @@ export default Ember.Controller.extend({
     return Ember.isEmpty(this.get('title'));
   }),
 
-  sortProperties: ['rating:desc', 'title:asc'],
+  sortBy: 'ratingDesc',
+
+  sortProperties: Ember.computed('sortBy', function () {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc'
+    };
+
+    return options[this.get('sortBy')].split(',');
+  }),
+
   sortedSongs: Ember.computed.sort('model.songs', 'sortProperties')
 });
