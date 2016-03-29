1d248ccd63e3938e65a31c5a7406e4e832dc4333
Peter Havens <peter.havens@gmail.com>
2016-03-29 13:02:42 -0600

    Add ability to search by song title.

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's'
+  },
   songCreationStarted: false,
   title: '',
+  searchTerm: '',
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function () {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function (song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
 
   actions: {
     enableSongCreation: function () {
@@ -47,5 +59,5 @@ export default Ember.Controller.extend({
     return options[this.get('sortBy')].split(',');
   }),
 
-  sortedSongs: Ember.computed.sort('model.songs', 'sortProperties')
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties')
 });
