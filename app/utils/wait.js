ba2b36618436000e20de9ea8da7dbef98c60ada8
Peter Havens <peter.havens@gmail.com>
2016-03-30 09:43:04 -0600

    Add "loading" templates, and test helper to simulate network latency.

---
+++ app/utils/wait.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+function isPromise(value) {
+  return value.then && typeof value.then === 'function'
+}
+
+export default function wait(value, delay) {
+  var promise = isPromise(value) ? value : Ember.RSVP.resolve(value);
+
+  return new Ember.RSVP.Promise(function (resolve) {
+    setTimeout(function () {
+      promise.then(function (result) {
+        resolve(result);
+      });
+    }, delay);
+  });
+}
